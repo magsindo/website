edba98fd4a693d7c0c34979ddc33b960f160472a
Rizky <yanandarizky@gmail.com>
2021-02-22 10:33:33 +0700

    firt commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    
+    <!--====== Title ======-->
+    <title>Magsindo</title>
+    
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="{{asset('assets/images/icon.png')}}" type="image/png">
+        
+    <!--====== Slick CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+        
+    <!--====== Font Awesome CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+        
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/LineIcons.css')}}">
+        
+    <!--====== Animate CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+        
+    <!--====== Magnific Popup CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+        
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    
+    <!--====== Default CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/default.css')}}">
+    
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    
+</head>
+
+@section('menu')
+@include('layouts.menu')
+@show
+
+@yield('content')
+
+
+    <!--====== Jquery js ======-->
+    <script src="{{asset('assets/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{asset('assets/js/vendor/modernizr-3.7.1.min.js')}}"></script>
+    
+    <!--====== Bootstrap js ======-->
+    <script src="{{asset('assets/js/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    
+    <!--====== Slick js ======-->
+    <script src="{{asset('assets/js/slick.min.js')}}"></script>
+    
+    <!--====== Isotope js ======-->
+    <script src="{{asset('assets/js/imagesloaded.pkgd.min.js')}}"></script>
+    <script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
+    
+    <!--====== Counter Up js ======-->
+    <script src="{{asset('assets/js/waypoints.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.counterup.min.js')}}"></script>
+    
+    <!--====== Circles js ======-->
+    <script src="{{asset('assets/js/circles.min.js')}}"></script>
+    
+    <!--====== Appear js ======-->
+    <script src="{{asset('assets/js/jquery.appear.min.js')}}"></script>
+    
+    <!--====== WOW js ======-->
+    <script src="{{asset('assets/js/wow.min.js')}}"></script>
+    
+    <!--====== Headroom js ======-->
+    <script src="{{asset('assets/js/headroom.min.js')}}"></script>
+    
+    <!--====== Jquery Nav js ======-->
+    <script src="{{asset('assets/js/jquery.nav.js')}}"></script>
+    
+    <!--====== Scroll It js ======-->
+    <script src="{{asset('assets/js/scrollIt.min.js')}}"></script>
+    
+    <!--====== Magnific Popup js ======-->
+    <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+    
+    <!--====== Main js ======-->
+    <script src="{{asset('assets/js/main.js')}}"></script>
+
+    {{-- fontawesome --}}
+    <script src="https://use.fontawesome.com/cd5f725593.js"></script>
+    
+</body>
+
+</html>
