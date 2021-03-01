0fc4e75faa584b83cfc95987a8ab4f68d4de487c
Rizky <yanandarizky@gmail.com>
2021-03-01 10:45:36 +0700

    add tawk to

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,5 +93,18 @@
 <script src="https://use.fontawesome.com/cd5f725593.js"></script>
 
 </body>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/603c4fa81c1c2a130d6366ae/1evlpeelp';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 
 </html>
