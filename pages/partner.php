84d650a1ff488b8610c6179fff556d813b3ae498
Rizky <yanandarizky@gmail.com>
2021-03-11 23:48:58 +0700

    modified partnership

--- pages/partner.php
+++ pages/partner.php
@@ -1,6 +1,47 @@
  <!--====== BRAND PART START ======-->
 
-    <div id="brand" class="brand-area pt-115">
+ <div id="brand" class="brand-area pt-115">
+     <div class="container">
+         <div class="row">
+             <ul class="hs full no-scrollbar">
+                <div class="fadeInUp" style="width: 215px">
+                    <div class="justify-content-center partnership">
+                        <img src="assets/images/partnership/DISHUBB.png" alt="">
+                    </div> 
+                </div>
+                <div class="fadeInUp" style="width: 215px">
+                    <div class="justify-content-center partnership">
+                        <img src="assets/images/partnership/BKSDAA.png" alt="">
+                    </div> 
+                </div>
+                <div class="fadeInUp" style="width: 215px">
+                    <div class="justify-content-center partnership">
+                        <img src="assets/images/partnership/KEMENKUMHAMM.png" alt="">
+                    </div> 
+                </div>
+                <div class="fadeInUp" style="width: 215px">
+                    <div class="justify-content-center partnership">
+                        <img src="assets/images/partnership/KOENERGIAA.png" alt="">
+                    </div> 
+                </div>
+                <div class="fadeInUp" style="width: 215px">
+                    <div class="justify-content-center partnership">
+                        <img src="assets/images/partnership/SCALEUPP.png" alt="">
+                    </div> 
+                </div>
+                <div class="fadeInUp" style="width: 215px"">
+                    <div class="justify-content-center partnership">
+                        <img src="assets/images/partnership/TESSOO.png" alt="">
+                    </div> 
+                </div>
+             </ul>
+         </div>
+     </div>
+ </div>
+
+
+
+ <!-- <div id="brand" class="brand-area pt-115">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-xl-5 col-lg-6 col-md-8 col-sm-9">
@@ -8,7 +49,7 @@
                             data-wow-delay="0.2s">
                             <h6 class="sub-title">Partnership</h6>
                             <h4 class="title">Partner <span>Magsindo</span></h4>
-                        </div> <!-- section title -->
+                        </div> 
                     </div>
                 </div>
             </div>
@@ -16,36 +57,36 @@
                 <div class="col-lg-2 col-md-12 wow fadeInUp">
                     <div class="justify-content-center partnership">
                         <img src="assets/images/partnership/DISHUBB.png" alt="">
-                    </div> <!-- single contact info -->
+                    </div> 
                 </div>
                 <div class="col-lg-2 col-md-12 wow fadeInUp">
                     <div class="justify-content-center partnership">
                         <img src="assets/images/partnership/BKSDAA.png" alt="">
-                    </div> <!-- single contact info -->
+                    </div> 
                 </div>
                 <div class="col-lg-2 col-md-12 wow fadeInUp">
                     <div class="justify-content-center partnership">
                         <img src="assets/images/partnership/TESSOO.png" alt="">
-                    </div> <!-- single contact info -->
+                    </div>
                 </div>
                 <div class="col-lg-2 col-md-12 wow fadeInUp">
                     <div class="justify-content-center partnership">
                         <img src="assets/images/partnership/SCALEUPP.png" alt="">
-                    </div> <!-- single contact info -->
+                    </div> 
                 </div>
                 <div class="col-lg-2 col-md-12 wow fadeInUp">
                     <div class="justify-content-center partnership">
                         <img src="assets/images/partnership/KOENERGIAA.png" alt="">
-                    </div> <!-- single contact info -->
+                    </div>
                 </div>
                 <div class="col-lg-2 col-md-12 wow fadeInUp">
                     <div class="justify-content-center partnership">
                         <img src="assets/images/partnership/KEMENKUMHAMM.png"
                             alt="">
-                    </div> <!-- single contact info -->
+                    </div>
                 </div>
-            </div> <!-- container -->
+            </div>
         </div>
-    </div>
+    </div> -->
 
-        <!--====== BRAND PART ENDS ======-->
+ <!--====== BRAND PART ENDS ======-->
