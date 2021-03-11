6efd7b8a6839eb198aad3c40d777ea5a77eebde3
Rizky <yanandarizky@gmail.com>
2021-03-11 23:52:05 +0700

    fixing

--- pages/partner.php
+++ pages/partner.php
@@ -2,41 +2,49 @@
 
  <div id="brand" class="brand-area pt-115">
      <div class="container">
-         <div class="row">
-             <ul class="hs full no-scrollbar">
-                <div class="fadeInUp" style="width: 215px">
-                    <div class="justify-content-center partnership">
-                        <img src="assets/images/partnership/DISHUBB.png" alt="">
-                    </div> 
-                </div>
-                <div class="fadeInUp" style="width: 215px">
-                    <div class="justify-content-center partnership">
-                        <img src="assets/images/partnership/BKSDAA.png" alt="">
-                    </div> 
-                </div>
-                <div class="fadeInUp" style="width: 215px">
-                    <div class="justify-content-center partnership">
-                        <img src="assets/images/partnership/KEMENKUMHAMM.png" alt="">
-                    </div> 
-                </div>
-                <div class="fadeInUp" style="width: 215px">
-                    <div class="justify-content-center partnership">
-                        <img src="assets/images/partnership/KOENERGIAA.png" alt="">
-                    </div> 
-                </div>
-                <div class="fadeInUp" style="width: 215px">
-                    <div class="justify-content-center partnership">
-                        <img src="assets/images/partnership/SCALEUPP.png" alt="">
-                    </div> 
-                </div>
-                <div class="fadeInUp" style="width: 215px"">
-                    <div class="justify-content-center partnership">
-                        <img src="assets/images/partnership/TESSOO.png" alt="">
-                    </div> 
-                </div>
-             </ul>
+         <div class="row justify-content-center">
+             <div class="col-xl-5 col-lg-6 col-md-8 col-sm-9">
+                 <div class="section-title text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
+                     <h6 class="sub-title">Partnership</h6>
+                     <h4 class="title">Partner <span>Magsindo</span></h4>
+                 </div>
+             </div>
          </div>
+         <div class="row pt-20">
+             <ul class="hs full no-scrollbar">
+                 <div class="fadeInUp" style="width: 215px">
+                     <div class="justify-content-center partnership">
+                         <img src="assets/images/partnership/DISHUBB.png" alt="">
+                     </div>
+                 </div>
+                 <div class="fadeInUp" style="width: 215px">
+                     <div class="justify-content-center partnership">
+                         <img src="assets/images/partnership/BKSDAA.png" alt="">
+                     </div>
+                 </div>
+                 <div class="fadeInUp" style="width: 215px">
+                     <div class="justify-content-center partnership">
+                         <img src="assets/images/partnership/KEMENKUMHAMM.png" alt="">
+                     </div>
+                 </div>
+                 <div class="fadeInUp" style="width: 215px">
+                     <div class="justify-content-center partnership">
+                         <img src="assets/images/partnership/KOENERGIAA.png" alt="">
+                     </div>
+                 </div>
+                 <div class="fadeInUp" style="width: 215px">
+                     <div class="justify-content-center partnership">
+                         <img src="assets/images/partnership/SCALEUPP.png" alt="">
+                     </div>
+                 </div>
+                 <div class="fadeInUp" style="width: 215px"">
+                    <div class=" justify-content-center partnership">
+                     <img src="assets/images/partnership/TESSOO.png" alt="">
+                 </div>
+         </div>
+         </ul>
      </div>
+ </div>
  </div>
 
 
