d9fa6a1e726ede1dd3d07ae9bb5335d409c90d28
Rizky <yanandarizky@gmail.com>
2021-03-19 23:14:44 +0700

    add foto komang

--- pages/team.php
+++ pages/team.php
@@ -1,154 +1,170 @@
  <!--==========================
       Team Section
     ============================-->
-        <section id="team" class="blog-area pt-115">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-4">
-                        <div class="section-title text-center pb-20 wow fadeInUp" data-wow-duration="1s"
-                            data-wow-delay="0.3s">
-                            <h6 class="sub-title">Our Team</h6>
-                            <h4 class="title">Tim <span>Magsindo Kreasi Multimedia</span></h4>
-                        </div> <!-- section title -->
-                    </div>
-                </div> <!-- row -->
+ <section id="team" class="blog-area pt-115">
+     <div class="container">
+         <div class="row justify-content-center">
+             <div class="col-lg-4">
+                 <div class="section-title text-center pb-20 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.3s">
+                     <h6 class="sub-title">Our Team</h6>
+                     <h4 class="title">Tim <span>Magsindo Kreasi Multimedia</span></h4>
+                 </div> <!-- section title -->
+             </div>
+         </div> <!-- row -->
 
-                
-                <div class="row justify-content-center">
-                    <div class="col-lg-3 col-md-6 col-xs-12 wow fadeInUp">
-                        <div class="member">
-                            <img src="assets/images/team/magrina.jpeg" loading="lazy" class="rounded-circle" alt="">
-                            <div class="member-info rounded-circle">
-                                <div class="member-info-content">
-                                    <h4>Magrina Rahayu</h4>
-                                    <span>CEO Magsindo Kreasi Multimedia</span>
-                                    <div class="social">
-                                        <a href=""><i class="fa fa-twitter"></i></a>
-                                        <a href=""><i class="fa fa-facebook"></i></a>
-                                        <a href=""><i class="fa fa-google-plus"></i></a>
-                                        <a href=""><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="row justify-content-center">
+         <div class="row justify-content-center">
+             <div class="col-lg-3 col-md-6 col-xs-12 wow fadeInUp">
+                 <div class="member">
+                     <img src="assets/images/team/magrina.jpeg" loading="lazy" class="rounded-circle" alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>Magrina Rahayu</h4>
+                             <span>CEO Magsindo Kreasi Multimedia</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
 
-                    <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="member">
-                            <img src="assets/images/team/heris.jpeg" loading="lazy" class="rounded-circle " alt="">
-                            <div class="member-info rounded-circle">
-                                <div class="member-info-content">
-                                    <h4>Heris Rizal</h4>
-                                    <span>Company Manager</span>
-                                    <div class="social">
-                                        <a href=""><i class="fa fa-twitter"></i></a>
-                                        <a href=""><i class="fa fa-facebook"></i></a>
-                                        <a href=""><i class="fa fa-google-plus"></i></a>
-                                        <a href=""><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+         <div class="row justify-content-center">
 
-                    <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.2s">
-                        <div class="member">
-                            <img src="assets/images/team/rizky.jpeg" loading="lazy" class="rounded-circle " alt="">
-                            <div class="member-info rounded-circle">
-                                <div class="member-info-content">
-                                    <h4>Rizky Yananda</h4>
-                                    <span>Chief Technology Officer</span>
-                                    <div class="social">
-                                        <a href=""><i class="fa fa-twitter"></i></a>
-                                        <a href=""><i class="fa fa-facebook"></i></a>
-                                        <a href=""><i class="fa fa-google-plus"></i></a>
-                                        <a href=""><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+             <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                 <div class="member">
+                     <img src="assets/images/team/heris.jpeg" loading="lazy" class="rounded-circle " alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>Heris Rizal</h4>
+                             <span>Company Manager</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
 
-                    <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.3s">
-                        <div class="member">
-                            <img src="assets/images/team/harish.jpeg" loading="lazy" class="rounded-circle " alt="">
-                            <div class="member-info rounded-circle">
-                                <div class="member-info-content">
-                                    <h4>Harish Ridha Prasaja</h4>
-                                    <span>System Analyst</span>
-                                    <div class="social">
-                                        <a href=""><i class="fa fa-twitter"></i></a>
-                                        <a href=""><i class="fa fa-facebook"></i></a>
-                                        <a href=""><i class="fa fa-google-plus"></i></a>
-                                        <a href=""><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+             <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.2s">
+                 <div class="member">
+                     <img src="assets/images/team/rizky.jpeg" loading="lazy" class="rounded-circle " alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>Rizky Yananda</h4>
+                             <span>Chief Technology Officer</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
 
-                </div>
-               
-                <div class="row justify-content-center">
+             <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.3s">
+                 <div class="member">
+                     <img src="assets/images/team/harish.jpeg" loading="lazy" class="rounded-circle " alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>Harish Ridha Prasaja</h4>
+                             <span>System Analyst</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
 
-                    <div class="col-lg-3 col-md-6 col-6 wow fadeInUp">
-                        <div class="member">
-                            <img src="assets/images/team/rizal.jpeg" loading="lazy" class="rounded-circle " alt="">
-                            <div class="member-info rounded-circle">
-                                <div class="member-info-content">
-                                    <h4>M. Rizal Rizwan</h4>
-                                    <span>Head Developer</span>
-                                    <div class="social">
-                                        <a href=""><i class="fa fa-twitter"></i></a>
-                                        <a href=""><i class="fa fa-facebook"></i></a>
-                                        <a href=""><i class="fa fa-google-plus"></i></a>
-                                        <a href=""><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+         </div>
 
-                    <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <div class="member">
-                            <img src="assets/images/team/filzha.jpeg" loading="lazy" class="rounded-circle " alt="">
-                            <div class="member-info rounded-circle">
-                                <div class="member-info-content">
-                                    <h4>Filzha Hidayat</h4>
-                                    <span>Graphic Designer</span>
-                                    <div class="social">
-                                        <a href=""><i class="fa fa-twitter"></i></a>
-                                        <a href=""><i class="fa fa-facebook"></i></a>
-                                        <a href=""><i class="fa fa-google-plus"></i></a>
-                                        <a href=""><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+         <div class="row justify-content-center">
 
-                    <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.2s">
-                        <div class="member">
-                            <img src="assets/images/team/fadllil.jpeg" loading="lazy" class="rounded-circle" alt="">
-                            <div class="member-info rounded-circle">
-                                <div class="member-info-content">
-                                    <h4>Fadllil Azhiimi</h4>
-                                    <span>Developer</span>
-                                    <div class="social">
-                                        <a href=""><i class="fa fa-twitter"></i></a>
-                                        <a href=""><i class="fa fa-facebook"></i></a>
-                                        <a href=""><i class="fa fa-google-plus"></i></a>
-                                        <a href=""><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+             <div class="col-lg-3 col-md-6 col-6 wow fadeInUp">
+                 <div class="member">
+                     <img src="assets/images/team/rizal.jpeg" loading="lazy" class="rounded-circle " alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>M. Rizal Rizwan</h4>
+                             <span>Head Developer</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
 
-                </div>
+             <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                 <div class="member">
+                     <img src="assets/images/team/filzha.jpeg" loading="lazy" class="rounded-circle " alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>Filzha Hidayat</h4>
+                             <span>Graphic Designer</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
 
-            </div>
-        </section><!-- #team -->
+             <div class="col-lg-3 col-md-6 col-6 wow fadeInUp" data-wow-delay="0.2s">
+                 <div class="member">
+                     <img src="assets/images/team/fadllil.jpeg" loading="lazy" class="rounded-circle" alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>Fadllil Azhiimi</h4>
+                             <span>Developer</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+
+         <div class="row justify-content-center">
+             <div class="col-lg-3 col-md-6 col-xs-12 wow fadeInUp">
+                 <div class="member">
+                     <img src="assets/images/team/komang.jpg" loading="lazy" class="rounded-circle" alt="">
+                     <div class="member-info rounded-circle">
+                         <div class="member-info-content">
+                             <h4>Vina Azizah</h4>
+                             <span>Marketing & Admin</span>
+                             <div class="social">
+                                 <a href=""><i class="fa fa-twitter"></i></a>
+                                 <a href=""><i class="fa fa-facebook"></i></a>
+                                 <a href=""><i class="fa fa-google-plus"></i></a>
+                                 <a href=""><i class="fa fa-linkedin"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+ </section><!-- #team -->
