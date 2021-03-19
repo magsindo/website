58208f2208a5d0a00be1239b1eef9bf4caed1405
Rizky <yanandarizky@gmail.com>
2021-03-19 23:46:57 +0700

    add multimedia services

--- pages/service.php
+++ pages/service.php
@@ -1,91 +1,87 @@
 <!--====== OUR SERVICE PART START ======-->
 
-    <section id="services" class="our-services-area pt-115">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-5 col-lg-6 col-md-8 col-sm-9">
-                    <div class="section-title text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
-                        <h6 class="sub-title">Our services</h6>
-                        <h4 class="title">Layanan Magsindo <span>untuk anda!</span></h4>
-                    </div> <!-- section title -->
-                </div>
-            </div> <!-- row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="our-services-tab pt-30">
-                        <ul class="nav justify-content-center wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s"
-                            id="myTab" role="tablist">
-                            <li class="nav-item">
-                                <a class="active" id="business-tab" data-toggle="tab" href="#business" role="tab"
-                                    aria-controls="business" aria-selected="true">
-                                    <i class="lni-display-alt"></i> <span>Software <br> Development</span>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a id="digital-tab" data-toggle="tab" href="#digital" role="tab" aria-controls="digital"
-                                    aria-selected="false">
-                                    <i class="lni-video"></i> <span>Multimedia <br></span>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="business" role="tabpanel"
-                                aria-labelledby="business-tab">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="our-services-content mt-45">
-                                            <h3 class="services-title">Pengembangan Aplikasi<span> untuk perkembangan
-                                                    bisnis anda.</span></h3>
-                                            <p class="text">Pembuatan aplikasi (berbasis web dan mobile (android/iOS))
-                                                merupakan salah satu produk utama kami<br>Dibuat oleh para ahli
-                                                dengan berbagai pengalaman yang siap untuk mewujudkan impian anda. Pengembangan aplikasi yang bisa PT. Magsindo Berikan: </p>
-                                            <li>Company Website</li>
-                                            <li>E-Campus Development</li>
-                                            <li>E-Goverment Development</li>
-                                            <li>E-Commerce Development</li>
-                                            <li>Networking Support</li>
-                                            <li>IT Training</li>
-                                        </div> <!-- our services content -->
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="our-services-image mt-50">
+<section id="services" class="our-services-area pt-115">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-5 col-lg-6 col-md-8 col-sm-9">
+                <div class="section-title text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
+                    <h6 class="sub-title">Our services</h6>
+                    <h4 class="title">Layanan Magsindo <span>untuk anda!</span></h4>
+                </div> <!-- section title -->
+            </div>
+        </div> <!-- row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="our-services-tab pt-30">
+                    <ul class="nav justify-content-center wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s"
+                        id="myTab" role="tablist">
+                        <li class="nav-item">
+                            <a class="active" id="business-tab" data-toggle="tab" href="#business" role="tab"
+                                aria-controls="business" aria-selected="true">
+                                <i class="lni-display-alt"></i> <span>Software <br> Development</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="digital-tab" data-toggle="tab" href="#digital" role="tab" aria-controls="digital"
+                                aria-selected="false">
+                                <i class="lni-video"></i> <span>Multimedia <br></span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="business" role="tabpanel"
+                            aria-labelledby="business-tab">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="our-services-content mt-45">
+                                        <h3 class="services-title">Pengembangan Aplikasi<span> untuk perkembangan
+                                                bisnis anda.</span></h3>
+                                        <p class="text">Pembuatan aplikasi (berbasis web dan mobile (android/iOS))
+                                            merupakan salah satu produk utama kami. Dibuat oleh para ahli
+                                            dengan berbagai pengalaman yang siap untuk mewujudkan impian anda.
+                                            Pengembangan aplikasi yang bisa PT. Magsindo Berikan: </p>
+                                        <li>Company Website</li>
+                                        <li>E-Campus Development</li>
+                                        <li>E-Goverment Development</li>
+                                        <li>E-Commerce Development</li>
+                                        <li>Networking Support</li>
+                                        <li>IT Training</li>
+                                    </div> <!-- our services content -->
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="our-services-image mt-50">
                                         <img src="assets/images/service/it.jpeg" loading="lazy" alt="">
-                                        </div> <!-- our services image -->
-                                    </div>
-                                </div> <!-- row -->
-                            </div>
+                                    </div> <!-- our services image -->
+                                </div>
+                            </div> <!-- row -->
+                        </div>
 
-                            <div class="tab-pane fade" id="digital" role="tabpanel" aria-labelledby="digital-tab">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="our-services-content mt-45">
-                                            <h3 class="services-title">Digital Marketing <span>for Your Business
-                                                    Growth.</span></h3>
-                                            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                                                nec est arcu. Maecenas semper tortor. <br> <br> In elementum in risus
-                                                sed commodo. Phasellus nisi ligula, luctus at tempor vitae, placerat at
-                                                ante. Cras sed consequat justo. Curabitur laoreet eu est vel convallis.
-                                            </p>
-
-                                            <!-- <div class="our-services-progress d-flex align-items-center mt-55">
-                                                <div class="circle" id="circles-2"></div>
-                                                <div class="progress-content">
-                                                    <h4 class="progress-title">Digital Marketing <br> Skill.</h4>
-                                                </div>
-                                            </div> -->
-                                        </div> <!-- our services content -->
+                        <div class="tab-pane fade" id="digital" role="tabpanel" aria-labelledby="digital-tab">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="our-services-content mt-45">
+                                        <h3 class="services-title">Digital Marketing <span>for Your Business
+                                                Growth.</span></h3>
+                                        <p class="text">
+                                            Multimedia menyediakan banyak cara agar orang bisa mendapatkan informasi dengan mudah dan baik. Multimedia ini bisa dimanfaatkan pada dunia bisnis, pendidikan dan lainnya. Berikut Layanan Multimedia PT. Magsindo Berikan Untuk anda:  
+                                        </p>
+                                        <li>Advertisement Video</li>
+                                        <li>Company Profile Video</li>
+                                        <li>Documentary Video</li>
+                                        <li>Graphic Design</li>
+                                    </div> <!-- our services content -->
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="our-services-image mt-50">
+                                        <img src="assets/images/service/it.jpeg" loading="lazy" alt="">
                                     </div>
-                                    <div class="col-lg-6">
-                                        <div class="our-services-image mt-50">
-                                            Under development
-                                            <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/V1wSFYEjqwo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-                                        </div> <!-- our services image -->
-                                    </div>
-                                </div> <!-- row -->
+                                </div> <!-- our services image -->
                             </div>
-                        </div> <!-- our services tab -->
+                        </div> <!-- row -->
                     </div>
-                </div> <!-- row -->
-            </div> <!-- container -->
-        </div>
-    </section>
+                </div> <!-- our services tab -->
+            </div>
+        </div> <!-- row -->
+    </div> <!-- container -->
+    </div>
+</section>
